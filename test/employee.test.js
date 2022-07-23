dca071e41d74608dcd1de4ea3abe0716af566232
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-07-23 18:11:14 -0500

    test for employee not working

--- test/employee.test.js
+++ test/employee.test.js
@@ -4,7 +4,7 @@ const Employee = require("../lib/employee");
 
 test("Can instantiate Employee instance", () => {
   const e = new Employee();
-  expect(e).toBe("object");
+  expect(typeof(e)).toBe("object");
 });
 
 test("Can set name via constructor arguments", () => {
